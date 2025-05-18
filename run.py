dce1effc9303fc0b82088cacf8cf7088f9b9d446
Kisejjere Rashid <rashidkisejjere0784@gmail.com>
2025-05-19 00:07:43 +0300

    removed streaming

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import os
 
-from app import app, socketio
+from app import app
 
 port = int(os.environ.get("FLASK_PORT", 5000))
 
 if __name__ == "__main__":
-    socketio.run(app,host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
