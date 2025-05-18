281a32071164e604d5fb36069305367a1933064c
Percy256 <semaganda.trevour@gmail.com>
2025-05-18 22:59:46 +0300

    Edits in the user model

--- app/auth/models.py
+++ app/auth/models.py
@@ -3,26 +3,33 @@
 from app import db
 
 # Define a base model for other database tables to inherit
+# This prevents code duplication for common fields like id, date_created, etc
+
+
 class Base(db.Model):
 
-    __abstract__  = True
+    __abstract__ = True  # This tells SQLAlchemy not to create a table for this class
 
-    id            = db.Column(db.Integer, primary_key=True)
-    date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
+    # Primary key column
+    id = db.Column(db.Integer, primary_key=True)
+
+    date_created = db.Column(db.DateTime,  default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
-                                           onupdate=db.func.current_timestamp())
+                              onupdate=db.func.current_timestamp())
 
 # Define a User model
+
+
 class User(Base):
 
     __tablename__ = 'user'
 
     # User Name
-    name    = db.Column(db.String(128),  nullable=False)
+    name = db.Column(db.String(128),  nullable=False)
 
     # Identification Data: email & password
-    email    = db.Column(db.String(128),  nullable=False,
-                                            unique=True)
+    email = db.Column(db.String(128),  nullable=False,
+                      unique=True)
     password = db.Column(db.String(192),  nullable=False)
 
     # Authorisation Data: role & status
@@ -31,10 +38,10 @@ class User(Base):
     # New instance instantiation procedure
     def __init__(self, name, email, password, location):
 
-        self.name     = name
-        self.email    = email
+        self.name = name
+        self.email = email
         self.password = password
         self.location = location
 
     def __repr__(self):
-        return '<User %r>' % (self.name) 
+        return '<User %r>' % (self.name)
